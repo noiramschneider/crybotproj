59f244661554eef4e19b677b9b537c713c409ef7
Marion Schneider <117548415+noiramschneider@users.noreply.github.com>
2025-02-18 10:37:22 -0500

    Update sketch.js
    changed spacing for secheresse

--- public/sketch.js
+++ public/sketch.js
@@ -537,7 +537,7 @@ function displayRegularScreen() {
   }
   fill(255);
   textSize(22);
-  text(`${t("soilDryness")} ${currentDryness}%`, 650, 465);
+  text(`${t("soilDryness")} ${currentDryness}%`, 635, 465);
  /* if (actualScore < 0) {
     fill(clicktextcolor);
     textSize(25);
